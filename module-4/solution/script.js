b8f7e2cdacf93153781e9b26443cef0253df1dd6
Pedro T <pedrotnns@gmail.com>
2022-08-09 14:55:50 -0600

    Version 1.1

--- module-4/solution/script.js
+++ module-4/solution/script.js
@@ -16,13 +16,3 @@
         }
     }
 })();
-
-(function(window) {
-    var helloSpeaker = new Object();
-    var speakWord = "Hello";
-    helloSpeaker.speak = function(name) {
-        console.log(speakWord + " " + name);
-    };
-    this.helloSpeaker = helloSpeaker;
-})(window);
-
