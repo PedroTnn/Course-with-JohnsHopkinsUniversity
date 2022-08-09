0979c2e91057262b3cf57d496b6475b0e268ac2e
Pedro T <pedrotnns@gmail.com>
2022-08-09 14:48:40 -0600

    Script Concat

--- module-4/solution/script.js
+++ module-4/solution/script.js
@@ -1,4 +1,11 @@
 (function() {
+    
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+   this.byeSpeaker = byeSpeaker;
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
@@ -9,3 +16,13 @@
         }
     }
 })();
+
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    this.helloSpeaker = helloSpeaker;
+})(window);
+
